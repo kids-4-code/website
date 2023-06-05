f7f82488fae3caa22cf0aea5203fa8a113bc55d6
Tyler Song <tyler.x.song@gmail.com>
2023-06-04 22:13:22 -0400

    added faq page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ["index.html", "./src/**/*.html"],
+	content: ["index.html", "*.html"],
 	theme: {
 		extend: {
 			colors: {
