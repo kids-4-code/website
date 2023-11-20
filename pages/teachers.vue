9fa2271a6aac4b78b9dee3295c6166d9ae1decc2
IS-2020 <71801701+IS-2020@users.noreply.github.com>
2023-11-20 17:17:00 -0500

    Update teachers.vue

--- pages/teachers.vue
+++ pages/teachers.vue
@@ -17,21 +17,24 @@ useHead({
 
         <div class="divide-y divide-gray-200 w-11/12 md:w-10/12 lg:w-9/12 flex flex-col">
           <div class="flex justify-center">
-            <TeacherCard name="Ishaan Samantray" title="Co-president"></TeacherCard>
-            <TeacherCard name="Tyler Song" title="Co-president"></TeacherCard>
+            <TeacherCard name="Ishaan Samantray" title="President"></TeacherCard>
+            <TeacherCard name="Tyler Song" title="Vice President"></TeacherCard>
 
           </div>
           <div class="flex justify-center">
-            <TeacherCard name="Aanya Kapoor" title="Secretary"></TeacherCard>
+            <TeacherCard name="Aanya Kapoor" title="Outreach Manager"></TeacherCard>
             <TeacherCard name="Haressh Nair" title="Secretary"></TeacherCard>
+            <TeacherCard name="____________" title="Finance Manager"></TeacherCard>
           </div>
           <div class="grid grid-cols-2 lg:grid-cols-4 mx-auto max-w-4xl">
-            <TeacherCard name="Joanne Li" title="Team Lead, Python"></TeacherCard>
+            <TeacherCard name="Bryan Yung" title="Team Lead, Python"></TeacherCard>
             <TeacherCard name="Ved Roychowdhury" title="Team Lead, Python"></TeacherCard>
             <TeacherCard name="Michael James" title="Team Lead, Java"></TeacherCard>
             <TeacherCard name="Maxwell Shawer" title="Team Lead, Java"></TeacherCard>
             <TeacherCard name="Siddhant Shah" title="Team Lead, HTML"></TeacherCard>
             <TeacherCard name="Albert Gao" title="Team Lead, HTML"></TeacherCard>
+            <TeacherCard name="Digonto Chatterjee" title="Team Lead, C++"></TeacherCard>
+            <TeacherCard name="Sudiksha Purohit" title="Team Lead, C++"></TeacherCard>
             <TeacherCard name="Kevin Xue" title="Team Lead, Intro to Programming"></TeacherCard>
             <TeacherCard name="Grant Kim" title="Team Lead, Intro to Programming"></TeacherCard>
           </div>
