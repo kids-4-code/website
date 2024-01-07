9d610391c250d4d7e55a274c9fc085339aeb8169
Tyler Song <tyler.x.song@gmail.com>
2024-01-07 17:07:17 -0500

    add descriptions for new positions

--- pages/apply.vue
+++ pages/apply.vue
@@ -32,7 +32,10 @@
                     <Icon name="tabler:calendar-check" class="text-md text-emerald-700"></Icon>
                     <span class="text-gray-700 text-sm font-sans font-semibold ml-1">Applications close</span>
                   </span>
-                  <p class="text-gray-700 text-xl font-body">February 22nd, 2024</p>
+                  <p class="text-gray-700 text-xl font-body">
+                    February
+                    7th,
+                    2024</p>
                 </div>
                 <div> <span class="flex items-center">
                     <Icon name="tabler:calendar-code" class="text-md text-emerald-700"></Icon>
@@ -68,22 +71,18 @@
               <hr class="mt-2 mx-2">
               <div class="p-2">
                 <h4 class="text-lg text-gray-700 font-serif font-semibold">Role</h4>
-                <p class="text-sm">
-                  Helps develop our website (kids4code.org), including designing new pages & updating existing ones.
-                  Experience in the following areas is prioritized:
-                  HTML/CSS (required)
-                  Web/UI design (highly recommended)
-                  Nice to haves:
-                  TailwindCSS 
-                  Vue.js (not super important but a nice plus)
-
-                </p>
-                <h4 class="text-lg text-gray-700 font-serif font-semibold mt-2">Skills</h4>
-                <p class="text-sm">
-                  More info coming soon!
-                </p>
+                <ul class="text-sm list-disc list-inside">
+                  <li>Helps develop our website, including designing new pages &amp; updating existing ones.</li>
+                  <li>Experience in the following areas is prioritized:</li>
+                  <ul class="text-sm list-disc list-inside ml-4">
+                    <li>HTML/CSS (required)</li>
+                    <li>Web/UI design (highly recommended)</li>
+                    <li>Vue.js (nice to have)</li>
+                    <li>TailwindCSS (not super important but a nice plus)</li>
+                  </ul>
+                </ul>
                 <div class="mt-6">
-                  <LinkButton disabled>Apply</LinkButton>
+                  <LinkButton to="https://forms.gle/XjVUnNWk6gghyp4U6">Apply</LinkButton>
                 </div>
               </div>
 
@@ -96,21 +95,16 @@
               <hr class="mt-2 mx-2">
               <div class="p-2">
                 <h4 class="text-lg text-gray-700 font-serif font-semibold">Role</h4>
-                <p class="text-sm">
-                 Have a comprehensive understanding of the language of choice.
-                 Be able to communicate with students professionally..
-                 Grade assignments of the previous week after the class ends.
-                 Works directly with students, teaching classes virtually over Zoom
-                 All classes are on weekends and run for around 45m - 1hr.
-                 Must teach at least 3 classes per session.
-
-                </p>
-                <h4 class="text-lg text-gray-700 font-serif font-semibold mt-2">Skills</h4>
-                <p class="text-sm">
-                  More info coming soon!
-                </p>
+                <ul class="text-sm list-disc list-inside">
+                  <li>Have a comprehensive understanding of the programming language of choice.</li>
+                  <li>Be able to communicate with students in a professional, but friendly manner.</li>
+                  <li>Grade assignments of the previous week after the class ends.</li>
+                  <li>Works directly with students, teaching classes virtually over Zoom</li>
+                  <li>All classes are on weekends and run for around 45m - 1hr.</li>
+                  <li>Must teach at least 3 classes per session.</li>
+                </ul>
                 <div class="mt-6">
-                  <LinkButton disabled>Apply</LinkButton>
+                  <LinkButton to="https://forms.gle/xKYcqgK7HrGykoTq6">Apply</LinkButton>
                 </div>
               </div>
 
@@ -123,27 +117,29 @@
               <hr class="mt-2 mx-2">
               <div class="p-2">
                 <h4 class="text-lg text-gray-700 font-serif font-semibold">Role</h4>
-                <p class="text-sm">
-                  Develop slides & homework assignments for new courses.
-                  Our classes are targeted at roughly 8-13-year-olds, so ensuring that concepts are explained in a simple & easy-to-understand way is crucial!
-                  The new courses we will be offering are:
-                  Java 3 (https://kids4code.org/courses/java-3)
-                  Roughly AP CSA level or a little bit below: if you took AP CSA you should have enough knowledge to help work on this course
-                  Python 3 (https://kids4code.org/courses/python-3)
-                  Intro to Machine Learning (more details coming soon!)
-                  We will focus on basic machine learning concepts in sci-kit-learn and possibly PyTorch if there is time.
-
-                </p>
-                <h4 class="text-lg text-gray-700 font-serif font-semibold mt-2">Skills</h4>
-                <p class="text-sm">
-                  More info coming soon!
-                </p>
+                <ul class="text-sm list-disc list-inside">
+                  <li>Develop slides &amp; homework assignments for new courses.</li>
+                  <li>Our classes are targeted at roughly 8-13-year-olds, so ensuring that concepts are explained in a
+                    simple
+                    &amp; easy-to-understand way is crucial!</li>
+                  <li>The new courses we will be offering are:</li>
+                  <ul class="ml-4 text-sm list-disc list-inside">
+                    <li>Java 3: roughly AP CSA level or a little bit below: if you took AP CSA you should have enough
+                      knowledge to help
+                      work on this course</li>
+                    <li>Python 3</li>
+                    <li>Intro to Machine Learning (more details coming soon!)
+                      We will focus on basic machine learning concepts in sci-kit-learn and possibly PyTorch if there is
+                      time.</li>
+                  </ul>
+                  <li>Syllabi can be found on our courses page.</li>
+                </ul>
                 <div class="mt-6">
-                  <LinkButton disabled>Apply</LinkButton>
+                  <LinkButton to="https://forms.gle/ZyyS26jSqfPkRyCQA">Apply</LinkButton>
                 </div>
               </div>
 
-                </div>
+            </div>
             <div class="flex flex-col border border-gray-200 rounded-lg p-6 gap-y-2">
               <div class="flex items-center gap-2 border-gray-200 rounded-md text-gray-700 p-2 pb-0">
                 <Icon name="tabler:school" class="text-3xl text-gray-700"></Icon>
@@ -152,27 +148,32 @@
               <hr class="mt-2 mx-2">
               <div class="p-2">
                 <h4 class="text-lg text-gray-700 font-serif font-semibold">Role</h4>
-                <p class="text-sm">
-                  Manages all the teachers in specific subjects. Oversees attendance of teachers within their course.
-                  Must have solid communication skills, be able to reach your teachers, and communicate effectively.
-                  Be able to fill in for classes with absent teachers.
-                  Requirement of 3 classes per session in any language.
-                  Grade assignments within the languages of choice.
-                  Be fluent in the language of choice.
-                  Working closely with Curriculum Developers, Teachers, and other team members, to smooth collaboration and communication
-                  The Team Lead plays a vital role in fostering a positive and productive working environment within the team.
-
-                </p>
-                <h4 class="text-lg text-gray-700 font-serif font-semibold mt-2">Skills</h4>
-                <p class="text-sm">
-                  More info coming soon!
-                </p>
+                <ul class="text-sm list-disc list-inside">
+                  <li>Manages all the teachers in specific subjects. Oversees attendance of teachers within their course.
+                  </li>
+                  <li>Must have solid communication skills, be able to reach your teachers, and communicate effectively.
+                  </li>
+                  <li>
+                    Be able to fill in for classes with absent teachers.
+                  </li>
+                  <li>
+                    Requirement of 3 classes per session in any language.
+                  </li>
+                  <li>Grade assignments within the languages of choice.</li>
+                  <li>Be fluent in the language of choice.</li>
+                  <li>Working closely with Curriculum Developers, Teachers, and other team members, to smooth
+                    collaboration
+                    and communication.</li>
+                  <li>The Team Lead plays a vital role in fostering a positive and productive working environment within
+                    the
+                    team.</li>
+                </ul>
                 <div class="mt-6">
-                  <LinkButton disabled>Apply</LinkButton>
+                  <LinkButton to="https://forms.gle/qXVaXaFFdDgr7VPh9">Apply</LinkButton>
                 </div>
               </div>
 
-                </div>
+            </div>
             <div class="flex flex-col border border-gray-200 rounded-lg p-6 gap-y-2">
               <div class="flex items-center gap-2 border-gray-200 rounded-md text-gray-700 p-2 pb-0">
                 <Icon name="tabler:school" class="text-3xl text-gray-700"></Icon>
@@ -181,26 +182,25 @@
               <hr class="mt-2 mx-2">
               <div class="p-2">
                 <h4 class="text-lg text-gray-700 font-serif font-semibold">Role</h4>
-                <p class="text-sm">
-                  Expanding our reach and impact within the community. 
-                  This role involves developing and implementing strategies to engage with schools, parents, and potential partners. 
-                  The Outreach Director is responsible for creating and maintaining relationships with teachers and promoting awareness of our coding programs. 
-                  Must have prior experience in communication (emailing & calling).
-                  Must have some advertising experience.
-                  Be able to write professional emails to parents, teachers, and students.
-                  Be able to communicate with partner organizations.
+                <ul class="text-sm list-disc list-inside">
+                  <li>Expanding our reach and impact within the community.</li>
+                  <li>This role involves developing and implementing strategies to engage with schools, parents, and
+                    potential
+                    partners.</li>
+                  <li>The Outreach Director is responsible for creating and maintaining relationships with teachers and
+                    promoting awareness of our coding programs.</li>
+                  <li>Must have prior experience in communication (emailing &amp; calling).
+                    Must have some advertising experience.</li>
+                  <li>Be able to write professional emails to parents, teachers, and students and communicate with partner
+                    organizations.</li>
 
-                </p>
-                <h4 class="text-lg text-gray-700 font-serif font-semibold mt-2">Skills</h4>
-                <p class="text-sm">
-                  More info coming soon!
-                </p>
+                </ul>
                 <div class="mt-6">
-                  <LinkButton disabled>Apply</LinkButton>
+                  <LinkButton to="https://forms.gle/qXVaXaFFdDgr7VPh9">Apply</LinkButton>
                 </div>
               </div>
 
-                </div>
+            </div>
             <div class="flex flex-col border border-gray-200 rounded-lg p-6 gap-y-2">
               <div class="flex items-center gap-2 border-gray-200 rounded-md text-gray-700 p-2 pb-0">
                 <Icon name="tabler:school" class="text-3xl text-gray-700"></Icon>
@@ -209,22 +209,21 @@
               <hr class="mt-2 mx-2">
               <div class="p-2">
                 <h4 class="text-lg text-gray-700 font-serif font-semibold">Role</h4>
-                <p class="text-sm">
-                  Oversee the finances of the organization. 
-                  Responsibilities include budgeting, financial planning, and ensuring the efficient use of resources. 
-                  The Finance Director works closely with the leadership team to make informed financial decisions that align with the nonprofit's mission and goals. 
-                  Responsibilities include paying for Zoom, managing advertising funds, and organizing fundraisers and bake sales.
-                  Must have prior experience handling money.
-                  Demonstrate a skill for accounting, and tracking expenditures.
-
-
-                </p>
-                <h4 class="text-lg text-gray-700 font-serif font-semibold mt-2">Skills</h4>
-                <p class="text-sm">
-                  More info coming soon!
-                </p>
+                <ul class="text-sm list-disc list-inside">
+                  <li>Oversee the finances of the organization.</li>
+                  <li>Responsibilities include budgeting, financial planning, and ensuring the efficient use of resources.
+                  </li>
+                  <li>The Finance Director works closely with the leadership team to make informed financial decisions
+                    that
+                    align with the nonprofit's mission and goals.</li>
+                  <li>Responsibilities include paying for Zoom &amp; other subscriptions, managing advertising funds, and
+                    organizing fundraisers.
+                  </li>
+                  <li>Should have prior experience handling money.</li>
+                  <li>Demonstrates a skill for accounting, and tracking expenditures.</li>
+                </ul>
                 <div class="mt-6">
-                  <LinkButton disabled>Apply</LinkButton>
+                  <LinkButton to="https://forms.gle/qXVaXaFFdDgr7VPh9">Apply</LinkButton>
                 </div>
               </div>
             </div>
