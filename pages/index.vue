7e068f2cc46190557d09da255fab9f2e4c4c4b8e
IS-2020 <71801701+IS-2020@users.noreply.github.com>
2026-04-12 19:23:27 -0400

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -9,7 +9,7 @@ useHead({
         <!--Hero-->
         <div class="pt-16 flex flex-col items-center text-center gap-2 w-6xl mx-2">
             <div class="mb-2">
-                <h3 class="font-sans font-semibold text-gray-300">Winter 2025 Session</h3>
+                <h3 class="font-sans font-semibold text-gray-300">Summer 2026 Session</h3>
                 <h1 class="font-serif text-4xl md:text-5xl text-gray-700 font-semibold">Online coding classes,</h1>
                 <h1 class="font-serif text-4xl md:text-5xl text-brand-600 font-semibold mb-0">100% free.</h1>
             </div>
@@ -297,7 +297,7 @@ useHead({
                     class="grid grid-cols-1 md:grid-cols-2 bg shadow-sm border-gray-200 gap-8 text-left border p-6 py-8 rounded-xl text-gray-700 z-0">
                     <div class="z-10">
                         <img class="w-16 mb-2 drop-shadow-sm" src="/images/icons/enroll.png">
-                        <h1 class="text-4xl font-serif font-semibold">Winter 2025</h1>
+                        <h1 class="text-4xl font-serif font-semibold">Summer 2026</h1>
                         <p class="text-sm md:w-3/4 block mt-1">Registration is now open!</p>
 
                         <LinkButton disabled class="mt-4" to="https://docs.google.com/forms/d/e/1FAIpQLSd1g-sSJHupLhMDCWbI4oi4hRhIoKNBuy2PT-Mj3KHgIaqKIQ/viewform" color="sky" newTab>
@@ -310,7 +310,7 @@ useHead({
                                 <Icon name="tabler:clock" class="text-md text-blue-700"></Icon>
                                 <span class="text-gray-700 text-sm font-sans font-semibold ml-1">Dates</span>
                             </span>
-                            <p class="text-gray-700 text-xl font-sans">February 1st — March 9th</p>
+                            <p class="text-gray-700 text-xl font-sans">Dates TBD:</p>
                         </div>
                         <div class="p-4 border border-gray-200 bg-white rounded-lg">
                             <span class="flex items-center">
@@ -335,7 +335,7 @@ useHead({
                             <Icon name="tabler:calendar-event" class="text-md text-emerald-700"></Icon>
                             <span class="text-gray-700 text-sm font-sans font-semibold ml-1">Registration closes</span>
                         </span>
-                        <p class="text-gray-700 text-xl font-sans">January 28th</p>
+                        <p class="text-gray-700 text-xl font-sans">TBD</p>
                     </div>
 
                 </div>
