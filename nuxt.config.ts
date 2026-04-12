aaae9ef1f5328509818ef0b3f105cbf085cfc84b
IS-2020 <71801701+IS-2020@users.noreply.github.com>
2026-04-12 19:37:43 -0400

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,17 +1,22 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-	devtools: { enabled: true },
-	css: ["~/assets/css/tailwind.css", "~/assets/css/fonts.css"],
-	modules: ["@nuxtjs/tailwindcss", "nuxt-icon", "@nuxt/content", "nuxt-gtag"],
-	content: {
-		documentDriven: true,
-	},
-	app: {
-		head: {
-			titleTemplate: "%s - Kids for Code",
-		},
-	},
-	gtag: {
-		id: "G-GJ6MQFJZ1V",
-	},
+    devtools: { enabled: true },
+    css: ["~/assets/css/tailwind.css", "~/assets/css/fonts.css"],
+    modules: ["@nuxtjs/tailwindcss", "nuxt-icon", "@nuxt/content", "nuxt-gtag"],
+    content: {
+        documentDriven: true,
+    },
+    app: {
+        head: {
+            titleTemplate: "%s – Kids for Code",
+        },
+    },
+    gtag: {
+        id: "G-GJ6MQFJZ1V",
+    },
+    nitro: {
+        prerender: {
+            failOnError: false,
+        }
+    },
 });
